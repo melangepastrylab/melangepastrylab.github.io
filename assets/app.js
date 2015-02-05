57e3f99d3cd8abbbe4b1da503fefc0f495ef1faa
Marcus Ericsson <mericsson@gmail.com>
2015-02-04 20:59:33 -0800

    try multiple boxes

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,126 @@
 var main = function() {
 
   // disable stripe button
-  var button = $('.stripe-button-el');
+  var button = $('#customButton');
   if (button) {
     button.prop('disabled', true);
   }
 
-  // metadta triggers
-  var updateMetadata = function() {
+  var getMetadata = function() {
     var metadata = {};
     metadata.fullName = $.trim($('#fullName').val());
     metadata.pickupDate = $('#pickupDate').val();
     metadata.mailingList = $('#mailingList').prop('checked');
+    return metadata;
+  }
+
+
+  // metadata triggers
+  var updateMetadata = function() {
+    var metadata = getMetadata();
 
     if (metadata.fullName.length > 0
        && metadata.pickupDate !== "NotEntered") {
 
       // set metadata
+         /*
       var stringified = JSON.stringify(metadata);
       $('input[name=metadata]').val(stringified);
+      */
 
       // re-enable submit button
-      $('.stripe-button-el').prop('disabled', false);
+      $('#customButton').prop('disabled', false);
     } else if (metadata.fullName.length === 0
         || metadata.pickupDate === "NotEntered") {
       // disable submit button
-      $('.stripe-button-el').prop('disabled', true);
+      $('#customButton').prop('disabled', true);
     }
   }
 
+  var getQuantity = function() {
+    return $('#quantity').val();
+  }
+
+  var getAmountCents = function() {
+    var quantity = getQuantity();
+    var amount = quantity * 1500; // cents
+    return amount;
+  }
+
+  /*
+   * use token and other args to call out to heroku server which wil call
+   * out to stripe to make payment
+   */
+  var onTokenRecv = function(token) {
+
+    /* token
+     * Object {id: "tok_15ShfjE9X8U61k3SjXKMqDfv", livemode: true, created: 1423112123, used: false, object: "token"…}card: Objectclient_ip: "107.192.0.177"created: 1423112123email: "mericsson@gmail.com"id: "tok_15ShfjE9X8U61k3SjXKMqDfv"livemode: trueobject: "token"type: "card"used: falseverification_allowed: true}
+     */
+
+    var metadata = getMetadata();
+
+    var data = {
+      amount: getAmountCents(),
+      description: 'i (spring 2015): a taste of macarons',
+     /* metadata: getMetadata(),*/
+      stripeToken: token.id,
+      stripeTokenType: token.type,
+      stripeEmail: token.email,
+      fullName: metadata.fullName,
+      pickupDate: metadata.pickupDate
+    }
+
+    // send to herokuapp
+    $.ajax({
+      type: 'POST',
+      url: 'https://melange-checkout.herokuapp.com/',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        location.href = '/success.html';
+      },
+      error: function(data) {
+        console.log(data);
+        location.href = 'error.html';
+      },
+    });
+  };
+
+
+    // get args off of page
+    /*
+    var fullName = $.trim($('#fullName').val());
+    var pickupDate = $('#pickupDate').val();
+    var mailingList = $('#mailingList').prop('checked');
+    var quantity = $('#quantity').val();
+    var amount = quantity * 1500; // cents
+    var key = 'pk_live_lUrKXKuFxSYeqGbtqsjnytWd';
+    */
+
+    var stripeHandler = StripeCheckout.configure({
+      key: 'pk_live_lUrKXKuFxSYeqGbtqsjnytWd',
+      image: '/assets/melange_logo_small.png',
+      token: function(token, args) {
+        onTokenRecv(token, args);
+      }
+    });
+
+    var openStripeHandler = function() {
+      stripeHandler.open({
+        name: 'melange pastry lab',
+        description: 'i (spring 2015): a taste of macarons',
+        amount: getAmountCents()
+      });
+    }
+
+
+  /* bindings */
   $('#fullName').keyup(updateMetadata);
   $('#pickupDate').change(updateMetadata);
   $('#mailingList').change(updateMetadata);
+  $('#customButton').on('click', openStripeHandler);
 
 };
 
